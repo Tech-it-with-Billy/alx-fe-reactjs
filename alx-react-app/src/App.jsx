789dd0abe354f91031b71df1af65fc8532ec9ae5
Billy Och <ochiengbilly02@gmail.com>
2025-07-18 13:46:16 +0300

    update files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage.jsx';
+import Header from './components/Header.jsx';
+import MainContent from './components/MainContent.jsx';
+import Footer from './components/Footer.jsx';
 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -10,7 +13,11 @@ function App() {
 
   return (
     <>
+    <Header />
     <WelcomeMessage />
+    <MainContent />
+    <Footer />
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
