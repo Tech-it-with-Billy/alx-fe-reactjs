342f20eafdc25ce9e411dbd83b10e19da4b6be01
Billy Och <ochiengbilly02@gmail.com>
2025-07-20 17:04:57 +0300

    adding alx-react-app-new react app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: '#62708cff', color: 'white', padding: '20px', textAlign: 'center' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
