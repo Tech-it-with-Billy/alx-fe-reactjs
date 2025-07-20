e55000f25e11023001acfcd8b58ed71a7d30bbcc
Billy Och <ochiengbilly02@gmail.com>
2025-07-20 17:24:53 +0300

    add Count.jsx file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
 import UserProfile from './components/UserProfile.jsx';
+import Counter from './components/Counter.jsx'; 
 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -16,6 +17,8 @@ function App() {
     <Header />
     <WelcomeMessage />
     <MainContent />
+    <Counter />
+    {/* <Counter /> */}
     <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
     <Footer />
     </>
