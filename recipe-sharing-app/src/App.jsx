6d800ed5b1c93ae660238814087e8d4cd7df1b81
Billy Och <ochiengbilly02@gmail.com>
2025-07-24 14:28:10 +0300

    update files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useRecipeStore } from './stores/recipeStore';
+import { useRecipeStore } from './components/recipeStore';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import { create } from 'zustand';
