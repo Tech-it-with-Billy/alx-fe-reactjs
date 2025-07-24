54533bd5fa18194986c586dcb7165d3594964bfb
Billy Och <ochiengbilly02@gmail.com>
2025-07-24 14:34:17 +0300

    update files

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,6 +5,15 @@ const useRecipeStore = create((set) => ({
     addRecipe: (newRecipe) => set((state) => ({recipes: [...state.recipes, newRecipe]
     })),
 
+    updateRecipe: (updatedRecipe) => set((state) => ({
+        recipes: state.recipes.map(recipe =>
+            recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+        )
+    })),
+    deleteRecipe: (id) => set((state) => ({
+        recipes: state.recipes.filter(recipe => recipe.id !== id)   
+    })),
+
     setRecipes: (recipes) => set({recipes})
 }));
 
