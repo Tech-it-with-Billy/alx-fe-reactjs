3939fd58ba23f5a729c8c8475f8763b655e42308
Billy Och <ochiengbilly02@gmail.com>
2025-08-17 14:50:03 +0300

    Update:implementing recipe card navigation

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import RecipeList from "../data.json";
+import { Link } from "react-router-dom";
 
 function HomePage() {
     const [recipes, setRecipes] = useState([]);
@@ -35,24 +36,20 @@ function HomePage() {
                 className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500"
             />
         </div>
-        <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {filteredRecipes.map((recipe) => (
-            <div
-                key={recipe.id}
-                className="bg-white rounded-xl shadow-md overflow-hidden transform transition duration-300 hover:shadow-xl hover:scale-105"
-            >
-                <img
-                    src={recipe.image}
-                    alt={recipe.title}
-                    className="w-full h-48 object-cover"
-                />
-                <div className="p-4">
-                    <h2 className="text-lg font-semibold text-gray-800 mb-2">{recipe.title}</h2>
-                    <p className="text-gray-600 text-sm">{recipe.summary}</p>
+                <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
+                <div className="bg-white rounded-xl shadow-md overflow-hidden transform transition duration-300 hover:shadow-xl hover:scale-105">
+                    <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover" />
+                    <div className="p-4">
+                        <h2 className="text-lg font-semibold text-gray-800 mb-2">{recipe.title}</h2>
+                        <p className="text-gray-600 text-sm">{recipe.summary}</p>
+                    </div>
                 </div>
-            </div>
+                </Link>
             ))}
         </div>
+
         </div>
     );
 }
